59a007f12f48a705352f99a731925bd3b3f7025a
Kiwi Browser <40272275+kiwibrowser@users.noreply.github.com>
2021-08-08 04:00:27 +0300

    Simplify PersonalizeResults user script injection

--- chrome/android/java/src/org/chromium/chrome/browser/PersonalizeResults.java
+++ chrome/android/java/src/org/chromium/chrome/browser/PersonalizeResults.java
@@ -29,13 +29,13 @@ public class PersonalizeResults {
           tab.getWebContents().evaluateJavaScript("(function(){ if (!document.location.href.includes('https://translate.google.com/translate_c')) { return; } var b=document.getElementById(\"gt-nvframe\");if(b){b.style.position='unset';document.body.style.top='0px'}else{var child=document.createElement('iframe');child.id='gt-nvframe';child.src=document.location.href.replace('/translate_c','/translate_nv');child.style.width='100%';child.style.height='93px';document.body.insertBefore(child,document.body.firstChild);var t=document.querySelector('meta[name=\"viewport\"]');if(!t){var metaTag=document.createElement('meta');metaTag.name='viewport';metaTag.content='width=device-width, initial-scale=1.0';document.body.appendChild(metaTag)}}})();", null);
        }
        if (tab != null && tab.getUrl().getSpec().startsWith("chrome-extension://")) {
-          tab.getWebContents().evaluateJavaScript("(function() { if (!document.location.href.includes('chrome-extension://')) { return; } window.addEventListener('load', function() { var t=document.querySelector('meta[name=\"viewport\"]');t&&(t.content=\"initial-scale=1\",t.content=\"width=device-width\"); if (!t) { var metaTag=document.createElement('meta'); metaTag.name='viewport'; metaTag.content='width=device-width, initial-scale=1.0'; document.body.appendChild(metaTag); } }); })();", null);
+          tab.getWebContents().evaluateJavaScript("(function() { if (!document.location.href.includes('chrome-extension://')) { return; } " + ADAPT_TO_MOBILE_VIEWPORT + "})();", null);
        }
        if (tab != null && tab.getUrl().getSpec().startsWith("chrome://")) {
-          tab.getWebContents().evaluateJavaScript("(function() { if (!document.location.href.includes('chrome://')) { return; } window.addEventListener('load', function() { var t=document.querySelector('meta[name=\"viewport\"]');t&&(t.content=\"initial-scale=1\",t.content=\"width=device-width\"); if (!t) { var metaTag=document.createElement('meta'); metaTag.name='viewport'; metaTag.content='width=device-width, initial-scale=1.0'; document.body.appendChild(metaTag); } }); })();", null);
+          tab.getWebContents().evaluateJavaScript("(function() { if (!document.location.href.includes('chrome://')) { return; } " + ADAPT_TO_MOBILE_VIEWPORT + "})();", null);
        }
        if (tab != null && tab.getUrl().getSpec().startsWith("kiwi://")) {
-          tab.getWebContents().evaluateJavaScript("(function() { if (!document.location.href.includes('kiwi://')) { return; } window.addEventListener('load', function() { var t=document.querySelector('meta[name=\"viewport\"]');t&&(t.content=\"initial-scale=1\",t.content=\"width=device-width\"); if (!t) { var metaTag=document.createElement('meta'); metaTag.name='viewport'; metaTag.content='width=device-width, initial-scale=1.0'; document.body.appendChild(metaTag); } }); })();", null);
+          tab.getWebContents().evaluateJavaScript("(function() { if (!document.location.href.includes('kiwi://')) { return; }" + ADAPT_TO_MOBILE_VIEWPORT + "})();", null);
        }
        if (tab != null && ContextUtils.getAppSharedPreferences().getBoolean("accept_cookie_consent", true) && (tab.getUrl().getSpec().startsWith("http://") || tab.getUrl().getSpec().startsWith("https://"))) {
           tab.getWebContents().evaluateJavaScript("(function(){function clickItem(elem) { elem.click(); } function acceptViaAPIs(){typeof window.__cmpui=='function'&&window.__cmpui('setAndSaveAllConsent',!0);typeof window.Didomi=='object'&&window.Didomi.setUserAgreeToAll()}window.globalObserver=null;function setupObserver(){if(!window.globalObserver){var newelem=document.createElement('style');newelem.innerHTML='.qc-cmp-showing { visibility: hidden !important; } body.didomi-popup-open { overflow: auto !important; } #didomi-host { visibility: hidden !important; }';document.body.appendChild(newelem);var MutationObserver=window.MutationObserver||window.WebKitMutationObserver;window.globalObserver=new MutationObserver(check);window.globalObserver.observe(window.document.documentElement,{childList:true,subtree:true});window.setTimeout(function(){window.globalObserver.disconnect();window.globalObserver=null},15000)}check()}function check(){window.setTimeout(function(){var listeners=[];listeners.push({selector:'#qcCmpUi',fn:acceptViaAPIs});listeners.push({selector:'#didomi-popup',fn:acceptViaAPIs});listeners.push({selector: '.accept-cookies-button,#purch-gdpr-banner__accept-button,#bbccookies-continue-button,.user-action--accept,.consent-accept,.bcpConsentOKButton,.button.accept,#footer_tc_privacy_button,button[aria-label=\"Button to collapse the message\"],.gdpr-form>.btn[value=\"Continue\"],button[on^=\"tap:\"][on$=\".accept\"],button[on^=\"tap:\"][on$=\".dismiss\"],.js-cookies-button,.app-offer__close_js,.lg-cc__button_type_action',fn: clickItem});for(var i=0,len=listeners.length,listener,elements;i<len;i++){listener=listeners[i];elements=window.document.querySelectorAll(listener.selector);for(var j=0,jLen=elements.length,element;j<jLen;j++){element=elements[j];if(!element.ready){element.ready=true;listener.fn.call(element, element)}}}},5)}window.addEventListener('DOMContentLoaded',setupObserver);check()})();", null);
@@ -96,6 +96,20 @@ public class PersonalizeResults {
 +"        }"
 +"    };"
 +"    makePropertyWritable(window, 'navigator', 'userAgent');"
++"})();";
+
+    private static final String ADAPT_TO_MOBILE_VIEWPORT = ""
++"(function() {"
++"window.addEventListener('load', function() {"
++"    var t = document.querySelector('meta[name=\"viewport\"]');"
++"    t && (t.content = 'initial-scale=1', t.content = 'width=device-width');"
++"    if (!t) {"
++"        var metaTag = document.createElement('meta');"
++"        metaTag.name = 'viewport';"
++"        metaTag.content = 'width=device-width, initial-scale=1.0';"
++"        document.body.appendChild(metaTag);"
++"    }"
++"});"
 +"})();";
 
     private static final String AMP_SCRIPT = ""
