bafa18b6d245d415bb7aaabd3416cbe24fda6f85
Kiwi Browser <40272275+kiwibrowser@users.noreply.github.com>
2021-08-11 02:33:07 +0300

    Update PersonalizeResults.java

--- chrome/android/java/src/org/chromium/chrome/browser/PersonalizeResults.java
+++ chrome/android/java/src/org/chromium/chrome/browser/PersonalizeResults.java
@@ -13,7 +13,7 @@ public class PersonalizeResults {
     public static void Execute(Tab tab) {
        final boolean shouldRewrapText = ContextUtils.getAppSharedPreferences().getBoolean("text_rewrap", false);
        final boolean shouldRemoveAmp = ContextUtils.getAppSharedPreferences().getBoolean("avoid_amp_websites", true);
-       if (tab != null && tab.getUrl().getSpec().startsWith("kiwi://version")) {
+       if (tab != null && tab.getUrl().getSpec().equals("chrome://version")) {
           tab.getWebContents().evaluateJavaScript("alert('Welcome to Kiwi Browser test version')", null);
        }
        if (shouldRemoveAmp && tab != null && IsSearchUrl(tab.getUrl().getSpec())) {
