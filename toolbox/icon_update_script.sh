468b79c2d056bbd19e18fc7be80c3d3ee32afdc8
Eduard Ursu <eduardursu78@gmail.com>
2021-08-12 00:22:05 +0200

    Update icon_update_script.sh

--- toolbox/icon_update_script.sh
+++ toolbox/icon_update_script.sh
@@ -1,10 +1,10 @@
 
-find ~/ -name $2 | while read filename
+find ./chrome ./components -name "$2" | while read filename
 do
-  width=`identify -format '%w' $filename`;
-  height=`identify -format '%w' $filename`;
+  width=$(identify -format '%w' "$filename");
+  height=$(identify -format '%w' "$filename");
   psize=0.16;
-  swidth=$(echo "scale=0;($width - $psize*$width)/1" | bc);
+ #swidth=$(echo "scale=0;($width - $psize*$width)/1" | bc);
   sheight=$(echo "scale=0;($height - $psize*$height)/1" | bc);
   echo " $filename ";
   if [ -z "$3" ]
@@ -12,9 +12,9 @@ do
         echo "\$3 is empty";
   else
         echo "Icon color parameter is NOT empty : $3";
-        sed -e "s/svg xmlns/svg fill='$3' xmlns/g" $1 > /tmp/tmp.svg
-        mv /tmp/tmp.svg $1
+        sed -e "s/svg xmlns/svg fill='$3' xmlns/g" "$1" > /tmp/tmp.svg
+        mv /tmp/tmp.svg "$1"
   fi
-  rsvg-convert $1 -h $sheight -o /tmp/tmp.png;
-  convert /tmp/tmp.png -background none -gravity center -extent $width'x'$height $filename;
+  rsvg-convert "$1" -h "$sheight" -o /tmp/tmp.png;
+  convert /tmp/tmp.png -background none -gravity center -extent "$width"'x'"$height" "$filename";
 done
