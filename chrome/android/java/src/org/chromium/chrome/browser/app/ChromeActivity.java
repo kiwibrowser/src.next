973ceeb51f0f68480eb1f7f6968839f46ae2a008
Delphox <smugleaf@live.com>
2021-08-11 10:16:30 -0300

    Fix personalizeresults execution

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -844,7 +844,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             }
 
             @Override
-            public void onPageLoadStarted(Tab tab, GURL url){
+            public void onUrlUpdated(Tab tab){
               FixDevToolsWindow.Execute(tab);
               PersonalizeResults.Execute(tab);
             }
