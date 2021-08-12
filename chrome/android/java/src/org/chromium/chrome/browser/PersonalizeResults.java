cacdf8ba3aec1418e76b5da3a71a893e726d4bea
Delphox <smugleaf@live.com>
2021-08-12 13:01:16 -0300

    again

--- chrome/android/java/src/org/chromium/chrome/browser/PersonalizeResults.java
+++ chrome/android/java/src/org/chromium/chrome/browser/PersonalizeResults.java
@@ -139,7 +139,7 @@ public class PersonalizeResults {
 +"  document.querySelectorAll('span[aria-label=\"AMP logo\"]').forEach(function(a) {"
 +"     a.style.display='none';"
 +"  });"
-+"  if (document.getElementsByClassName('amp-cantxt').length >= 1 && document.location.href.match(/\/amp[\/|\.]/)) { document.location.replace(document.getElementsByClassName('amp-cantxt')[0].innerText); }"
++"  if (document.getElementsByClassName('amp-cantxt').length >= 1 && document.location.href.match(/\\/amp[\\/|\\.]/)) { document.location.replace(document.getElementsByClassName('amp-cantxt')[0].innerText); }"
 +"  if (document.location.href.match(/\\/amp[\\/|\\.]/) && document.querySelector('head > link[rel=\"canonical\"]') != null && document.querySelector('head > link[rel=\"canonical\"]').href != document.location.href) { document.location.replace(document.querySelector('head > link[rel=\"canonical\"]').href); };"
 +"}"
 +""
